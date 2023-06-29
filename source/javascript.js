682e609c4028d9ea2348a94f86ebb9113b083de1
Blakely Mayhall <blakely.c.mayhall@gmail.com>
2023-06-28 21:54:01 -0500

    finally got everything working as expected - now to testing

--- source/javascript.js
+++ source/javascript.js
@@ -1,3 +1,4 @@
+const buttons       = document.querySelectorAll('button');
 const digitButtons  = document.querySelectorAll('.digitButton');
 const opButtons     = document.querySelectorAll('.opButton');
 const funcButtons   = document.querySelectorAll('.funcButton');
@@ -5,20 +6,11 @@ const MAX_DIGITS    = 8;
 const CalculatorModes = {
 	INIT: Symbol("init"),
 	NUM1_ENTRY: Symbol("num1_entry"),
-    AWAIT_NUM2: Symbol("await_num2"),
+    NUM2_STDBY: Symbol("num2_stdby"),
 	NUM2_ENTRY: Symbol("num2_entry"),
-	DISP_RESULT: Symbol("disp_result")
+	ENTR_STDBY: Symbol("entr_stdby")
 }
 
-/*
-
-Write the process by which if the user enters a digit after enter is sent, 
-overwrite the / re-init
-
-Need to error check this code, I think there is another bug somewhere
-    - Fix the fact you cant type mulitple digits after first input
-
-*/
 
 let num1 = null;
 let num2 = null;
@@ -28,7 +20,8 @@ let calculatorMode = CalculatorModes.INIT;
 
 digitButtons.forEach((digitButton) => {
     digitButton.addEventListener('click', () => {
-        if(calculatorMode === CalculatorModes.INIT || calculatorMode === CalculatorModes.DISP_RESULT) {
+
+        if (calculatorMode === CalculatorModes.INIT || calculatorMode === CalculatorModes.ENTR_STDBY) {
             num1 = null;
             num2 = null;
             operator = null;
@@ -37,8 +30,7 @@ digitButtons.forEach((digitButton) => {
             calculatorMode = CalculatorModes.NUM1_ENTRY;
         }
 
-        if (calculatorMode === CalculatorModes.AWAIT_NUM2 || calculatorMode === CalculatorModes.DISP_RESULT) {
-            dispNumber = "";
+        if (calculatorMode === CalculatorModes.NUM2_STDBY) {
             deleteDigitsFromScreen();
             calculatorMode = CalculatorModes.NUM2_ENTRY;
         }
@@ -52,37 +44,44 @@ digitButtons.forEach((digitButton) => {
 
 opButtons.forEach((opButton) => {
     opButton.addEventListener('click', () => {
-        if (calculatorMode != CalculatorModes.AWAIT_NUM2) {
-            if (calculatorMode === CalculatorModes.NUM1_ENTRY) {
-                num1 = +dispNumber;
-                deleteDigitsFromScreen();
-                calculatorMode = CalculatorModes.AWAIT_NUM2;
-            }
-            else if (calculatorMode === CalculatorModes.NUM2_ENTRY) {
-                num2 = +dispNumber;
-                num1 = operate(num1,num2,operator);
-                dispNumber = num1.toString();
-                deleteDigitsFromScreen();
-                addDigitsToScreen(dispNumber);
-                calculatorMode = CalculatorModes.AWAIT_NUM2;
-            }
+
+        if(calculatorMode === CalculatorModes.ENTR_STDBY) {
+            calculatorMode = CalculatorModes.NUM1_ENTRY;
         }
+
+        if (calculatorMode === CalculatorModes.NUM1_ENTRY) {
+            num1 = +dispNumber;
+            dispNumber = "";
+        }
+
+        if (calculatorMode === CalculatorModes.NUM2_ENTRY) {
+            num2 = +dispNumber;
+            num1 = operate(num1, num2, operator)
+            dispNumber = num1.toString();
+            deleteDigitsFromScreen();
+            addDigitsToScreen(dispNumber);
+        }
+
+        dispNumber = "";
         operator = opButton.dataset.operator;
+        calculatorMode = CalculatorModes.NUM2_STDBY;
     });
 });
 
 funcButtons.forEach((funcButton) => {
     funcButton.addEventListener('click', () => {
-        if (calculatorMode !== CalculatorModes.AWAIT_NUM2) {
-            if (funcButton.dataset.func == "enter") {
-                num2 = +dispNumber;
-                num1 = operate(num1,num2,operator);
-                dispNumber = num1.toString();
-                deleteDigitsFromScreen();
-                addDigitsToScreen(dispNumber);
-                calculatorMode = CalculatorModes.DISP_RESULT;
+        if (funcButton.dataset.func == "enter" && calculatorMode === CalculatorModes.NUM2_ENTRY) {
+            num2 = +dispNumber;
+            num1 = operate(num1, num2, operator);
+            if (num1 % 1 !== 0) {
+                num1 = num1.toFixed(2);
             }
-        }  
+            dispNumber = num1.toString();
+            deleteDigitsFromScreen();
+            addDigitsToScreen(dispNumber);
+            calculatorMode = CalculatorModes.ENTR_STDBY;
+        }
+         
 
         if (funcButton.dataset.func == "clear") {
             deleteDigitsFromScreen();
@@ -93,6 +92,10 @@ funcButtons.forEach((funcButton) => {
 
 // Support function - Adds digits to the screen from a string
 function addDigitsToScreen(dispNumber) {
+    if (dispNumber.length > 8) {
+        dispNumber = "OVERFLW"
+    } 
+
     for (let ii = 0; ii < dispNumber.length; ii++) {
         const digit = document.createElement("p");
         digit.textContent = dispNumber[ii];
